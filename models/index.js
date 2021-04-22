b5afb0f98db91047e116b625b95b2e70ae43137e
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-22 17:18:44 -0400

    reformatted models/index.js for readability

--- models/index.js
+++ models/index.js
@@ -5,17 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category,{
-  foriegnKey: 'category_id'
-})
+Product.belongsTo(Category,{ foriegnKey: 'category_id' });
+
 // Categories have many Products
-Category.hasMany(Product, {
-  foriegnKey: 'category_id'
-})
+Category.hasMany(Product, { foriegnKey: 'category_id' });
+
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: ProductTag })
+Product.belongsToMany(Tag, { through: ProductTag });
+
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product,{through: ProductTag })
+Tag.belongsToMany(Product, { through: ProductTag });
 
 
 module.exports = {
