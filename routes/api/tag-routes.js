293d2fc3b86310c5440bec312394594c2b872f52
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-24 01:55:55 -0400

    finished all routes
    may still need debugging, after debugging only README + video left

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,8 +34,11 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try{
-    const tagData = await Tag.create()
-  }
+    const tagData = await Tag.create(req.body)
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+}
 });
 
 router.put('/:id', async (req, res) => {
@@ -45,24 +48,27 @@ router.put('/:id', async (req, res) => {
     const tagData = await Tag.update({tag_name: req.body.tag_name},{where: {id: req.params.id}})
     console.log(req.body.tag_name)
     if(!tagData){
-      copnsole.log(2)
+      console.log(2)
       res.status(404).json({ message:'No location found with this id!' })
       return;
-    } else{
-      //  console.log ('successfully updated tag_name')
-       
+    } else {
       return res.status(200).json(tagData) 
     }
-  }catch (err) {
+  } catch (err) {
     res.status(500).json(err)
-}
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete one tag by its `id` value
   try{
-    const tagData = await Tag.destroy(req.params.id)
-  }
+    const tagData = await Tag.destroy( {where: {id: req.params.id}});
+    if (!tagData){
+      res.status(404).json({message: 'No location found with this id!'});
+    }
+  } catch (err) {
+    res.status(500).json(err)
+}
 });
 
 module.exports = router;
